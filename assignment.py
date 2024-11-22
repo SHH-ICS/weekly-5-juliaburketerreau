02dc5fd15bd83cc29612343867bc4878d4fb74eb
Julia Burke-Terreau <julia.burke-terreau@stu.ocsb.ca>
2024-11-22 15:42:59 +0000

    off by one

--- assignment.py
+++ assignment.py
@@ -2,8 +2,7 @@ number_of_digits = int(input())
 def calculatePi(digits):
   result = 0
   for i in range (number_of_digits):
-    ori = ((-1)**0)/(2*0+1)*4
-    result = ((((-1)**number_of_digits))/(2*number_of_digits + 1) * 4) + ori
+    result = (result+((-1)**i)/(2*i+1)*4) 
   return result
 
 print(calculatePi(number_of_digits))
